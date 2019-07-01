144c7e684537a54474761c7927b4e33ecd0ff3b7
Anand <anandkrish894@gmail.com>
2019-07-01 23:06:40 +0900

    Added UNet Model

--- blocks/convnorm.py
+++ blocks/convnorm.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 from torch.nn.utils.spectral_norm import spectral_norm
 
-class ConvNormPool(nn.Module):
+class ConvNorm(nn.Module):
     """
     A simply block consisting of a convolution layer,
     a normalization layer, ReLU activation and a pooling
@@ -9,8 +9,8 @@ class ConvNormPool(nn.Module):
     ResNet architecture.
     """
 
-    def __init__(self, in_channels, out_channels,norm='BN', act='ReLU', **kwargs):
-        super(ConvNormPool, self).__init__()
+    def __init__(self, in_channels, out_channels,norm='BN', **kwargs):
+        super(ConvNorm, self).__init__()
 
         if 'conv_args' in kwargs:
             kernel_size, stride,padding, bias = kwargs['conv_args']
@@ -31,33 +31,21 @@ class ConvNormPool(nn.Module):
             self.norm = nn.InstanceNorm2d(out_channels)
         elif norm == 'GN':
             self.norm = nn.GroupNorm(16, out_channels)
-        elif norm == 'SN':
-            self.conv = spectral_norm(self.conv)
         else:
             if norm != 'BN':
                 raise UserWarning('Undefined normalization '+norm+'. Using BatchNorm instead.')
             self.norm = nn.BatchNorm2d(out_channels)
 
-        if act == 'LeakyReLU':
-            self.act = nn.LeakyReLU(inplace=True)
-        else:
-            self.act = nn.ReLU(inplace=True)
-
-        if 'pool_args' in kwargs:
-            kernel_size, stride,padding, bias = kwargs['pool_args']
-            self.maxpool = nn.MaxPool2d(kernel_size=kernel_size,
-                                  stride=stride,
-                                  padding=padding)
-
-        else:
-            self.maxpool = nn.MaxPool2d(kernel_size=3,
-                                       stride=2, padding=1)
 
     def forward(self, input):
-        if self.norm_type == 'SN':
-            x = self.conv(input)
-        else:
-            x = self.norm(self.conv(input))
-        x = self.act(x)
-        x = self.maxpool(x)
+        x = self.norm(self.conv(input))
         return x
+
+if __name__ == "__main__":
+    import torch
+    u = ConvNorm(3, 10,conv_args=(3,1,0, True))
+
+    inp = torch.randn(32,3,128,128) #M x C x H x W
+    u.train()
+    result = u(inp)
+    print(result.size())
---
+++ models/unet.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn
+from numpy import rint
+import torch.nn.functional as F
+from NeuralBlocks.blocks.convnorm import ConvNorm
+
+
+class unetDown(nn.Module):
+    def __init__(self, in_channels, out_channels, norm='BN'):
+        super(unetDown, self).__init__()
+        self.layer = nn.Sequential(
+                    ConvNorm(in_channels, out_channels, norm, conv_args=(3, 1, 0, True)),
+                    nn.ReLU(inplace=True),
+                    ConvNorm(out_channels, out_channels, norm, conv_args=(3, 1, 0, True)),
+                    nn.ReLU(inplace=True))
+
+    def forward(self, input):
+        return self.layer(input)
+
+class unetUp(nn.Module):
+    def __init__(self, in_channels, out_channels, is_deconv):
+        super(unetUp, self).__init__()
+        self.conv = unetDown(in_channels, out_channels)
+
+        if is_deconv:
+            self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+        else:
+            self.up = nn.UpsamplingBilinear2d(scale_factor=2)
+
+    def forward(self, input1, input2):
+        output2 = self.up(input2)
+        offset = output2.size(2) - input1.size(2)
+        padding = 2*[int(offset/2), offset//2]
+        #print(rint(offset/2), int(offset/2), padding)
+
+        output1 = F.pad(input1, padding)
+        merged_output = torch.cat([output1, output2],1)
+        return self.conv(merged_output)
+
+class UNet(nn.Module):
+    def __init__(self, in_channels, n_class, norm = 'BN', filters = None, filter_scale = 4, is_deconv = True):
+
+        super(UNet,self).__init__()
+
+        self.is_deconv = is_deconv
+
+        if filters is None:
+            filters = [64,128,256,512,1024]
+
+        if len(filters) < 3:
+            raise ValueError('Number filters must be at least 3.')
+
+        filters = [int(f/filter_scale) for f in filters]
+        filters.insert(0, in_channels)
+
+        modules= []
+        # Downsampling phase
+        for i in range(1, len(filters)-1):
+            modules.append(unetDown(filters[i-1], filters[i]))
+            modules.append(nn.MaxPool2d(kernel_size=2))
+
+        modules.append(ConvNorm(filters[-2], filters[-1], 'BN', conv_args=(3,1,0, True)))
+
+        self.down_layers = nn.ModuleList(modules)
+
+        modules = []
+        for i in range(len(filters)-1,1,-1):
+            modules.append(unetUp(filters[i], filters[i-1], self.is_deconv))
+
+        self.up_layers = nn.ModuleList(modules)
+        self.final = nn.Conv2d(filters[1], n_class, 1)
+
+    def forward(self, input):
+
+        tensors = [input]
+        for i, module in enumerate(self.down_layers):
+            tensors.append(module(tensors[i]))
+
+        result = tensors[-1]
+        for i, module in enumerate(self.up_layers):
+            result = module(tensors[-(2*i +3)], result)
+        return self.final(result)
+
+
+if __name__ == "__main__":
+    u = UNet(3, 10)
+
+    inp = torch.randn(32,3,512,512) #M x C x H x W
+    u.train()
+    result = u(inp)
+
+
+
+
+
+
+
+
+
+
+
+
+
