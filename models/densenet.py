b6d9368665d88d67242e36422b20dcceaa43b146
Anand <anandkrish894@gmail.com>
2019-07-05 00:27:44 +0900

    Updated VGG

--- models/densenet.py
+++ models/densenet.py
@@ -1,2 +1,46 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DenseLayer(nn.Module):
+    def __init__(self, num_features, growth_rate, bn_size, drop_rate):
+        super(DenseLayer, self).__init__()
+        self.layer1 = nn.Sequential(
+                        nn.BatchNorm2d(num_features),
+                        nn.ReLU(inplace=True),
+                        nn.Conv2d(num_features, bn_size*growth_rate,
+                                 kernel_size=1, stride=1, bias=False)
+                        )
+
+        self.layer2 = nn.Sequential(
+                        nn.BatchNorm2d(bn_size*growth_rate),
+                        nn.ReLU(inplace=True),
+                        nn.Conv2d(bn_size*growth_rate, growth_rate,
+                                           kernel_size=3, stride=1, padding=1,
+                                           bias=False))
+        self.drop_rate = drop_rate
+
+    def forward(self, input):
+        x = torch.cat(input,1)
+        x = self.layer1(x)
+        x = self.layer2(x)
+
+        if self.drop_rate > 0:
+            x = F.dropout(x, p =self.drop_rate, training=self.training)
+
+        return x
+
+class DenseBlock(nn.Module):
+    def __init__(self, num_layers, num_features, bn_size, growth_rate, drop_rate):
+        super(DenseBlock, self).__init__()
+        for i in range(num_layers):
+            layer = DenseLayer(num_features + i*growth_rate, growth_rate=growth_rate,
+                               bn_size=bn_size, drop_rate=drop_rate)
+            self.add_module('denselayer{}'.format(i+1), layer)
+
+    def forward(self, input):
+        fea
+
+
 class DenseNet:
     pass
---
+++ models/vgg.py
@@ -0,0 +1,86 @@
+import torch.nn as nn
+from NeuralBlocks.blocks.meanspectralnorm import MeanSpectralNormConv2d
+
+cfgs = {
+    11: [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    13: [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    16: [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    19: [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+class VGG(nn.Module):
+    def __init__(self, model_config, in_channels, num_class, norm = 'BN', init_weights=True):
+        super(VGG, self).__init__()
+        layers= []
+
+        if not isinstance(model_config, list):
+            if model_config in [11,13,16,19]:
+                model_config = cfgs[model_config]
+            else:
+                raise ValueError("Invalid model_config. Must be a list of configs or a value"
+                                 "in [11,13,16,19]")
+
+
+        for v in model_config:
+            if v == 'M':
+                layers += [nn.MaxUnpool2d(kernel_size=2, stride=2)]
+            else:
+                if norm == 'MSN':
+                    conv2d = MeanSpectralNormConv2d(in_channels, v, kernel_size=3, padding=1)
+                    layers += [conv2d]
+                elif norm != 'IN':
+                    conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+                    layers += [conv2d, nn.InstanceNorm2d(v)]
+                elif norm != 'GN':
+                    conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+                    layers += [conv2d, nn.GroupNorm(16,v)]
+                else:
+                    if norm != 'BN':
+                        raise UserWarning('Undefined normalization ' + norm + '. Using BatchNorm instead.')
+                    conv2d = nn.Conv2d(in_channels,v, kernel_size=3, padding=1)
+                    layers += [conv2d, nn.BatchNorm2d(v)]
+                layers += [nn.ReLU(inplace=True)]
+
+                in_channels = v
+        self.features = nn.Sequential(*layers)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((7,7))
+
+        self.classifier = nn.Sequential(
+                            nn.Linear(512*7*7,4096),
+                            nn.ReLU(True),
+                            nn.Dropout(),
+                            nn.Linear(4096,4096),
+                            nn.ReLU(True),
+                            nn.Dropout(),
+                            nn.Linear(4096, num_class))
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, input):
+        x = self.features(input)
+        x = self.avgpool(x)
+        x = x.view(x.size(0),-1)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, MeanSpectralNormConv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+                if m.conv.bias is not None:
+                    nn.init.constant_(m.conv.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
