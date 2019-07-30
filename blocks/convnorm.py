a08f1f2b9d5e3f89f756ef27f19e3c9ce57c9cd9
Anand <anandkrish894@gmail.com>
2019-07-30 18:54:04 +0900

    Added Inplace ABN

--- blocks/convnorm.py
+++ blocks/convnorm.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+from inplace_abn import ABN
 from NeuralBlocks.blocks.meanspectralnorm import MeanSpectralNormConv2d
 from NeuralBlocks.blocks.meanweightnorm import MeanWeightNormConv2d
 from NeuralBlocks.blocks.spectralnorm import SpectralNormConv2d
@@ -11,9 +12,9 @@ class ConvNorm(nn.Module):
                  bias = True, padding_mode='zeros', norm = 'BN', groups_size=16, conv_last = False):
         super(ConvNorm, self).__init__()
 
-        if norm not in [None,'BN', 'IN', 'GN', 'LN','WN', 'SN', 'MWN','MSN', 'MSNTReLU', 'MWNTReLU']:
+        if norm not in [None,'BN', 'ABN','IN', 'GN', 'LN','WN', 'SN', 'MWN','MSN', 'MSNTReLU', 'MWNTReLU']:
             raise ValueError("Undefined norm value. Must be one of "
-                             "[None,'BN', 'IN', 'GN', 'LN', 'WN', 'SN','MWN', 'MSN', 'MSNTReLU', 'MWNTReLU']")
+                             "[None,'BN', 'ABN','IN', 'GN', 'LN', 'WN', 'SN','MWN', 'MSN', 'MSNTReLU', 'MWNTReLU']")
         layers = []
         if norm in ['MSN','MSNTReLU']:
             conv2d = MeanSpectralNormConv2d(in_channels, out_channels, kernel_size,
@@ -55,6 +56,11 @@ class ConvNorm(nn.Module):
                  stride, padding, dilation, groups,
                  bias, padding_mode)
             layers += [conv2d, nn.BatchNorm2d(out_channels) ]
+        elif norm == 'ABN':
+            conv2d = nn.Conv2d(in_channels, out_channels, kernel_size,
+                 stride, padding, dilation, groups,
+                 bias, padding_mode)
+            layers += [conv2d, ABN(out_channels) ]
         else:
             conv2d = nn.Conv2d(in_channels, out_channels, kernel_size,
                                stride, padding, dilation, groups,
@@ -68,10 +74,11 @@ class ConvNorm(nn.Module):
             So to change the order, we simply rotate the array by 1 to the 
             left and change the num_features to the in_channels size
         """
-        if conv_last and norm not in [None, 'MSN', 'SN','WN', 'WNTReLU', 'MSNTReLU']:
+        if conv_last:
             layers = layers[1:] + layers[:1]
             # Reinitialize the batchnorm layer or its variants
-            layers[0].__init__(in_channels)
+            if norm in ['BN', 'LN', 'IN', 'GN']:
+                layers[0].__init__(in_channels)
 
         self.layers= nn.Sequential(*layers)
 
--- blocks/convnormrelu.py
+++ blocks/convnormrelu.py
@@ -1,10 +1,7 @@
 import torch.nn as nn
-from NeuralBlocks.blocks.meanspectralnorm import MeanSpectralNormConv2d
+from NeuralBlocks.blocks.convnorm import ConvNorm
 from NeuralBlocks.blocks.meanspectralnorm import MeanSpectralNormConvReLU
-from NeuralBlocks.blocks.meanweightnorm import MeanWeightNormConv2d
 from NeuralBlocks.blocks.meanweightnorm import MeanWeightNormConvReLU
-from NeuralBlocks.blocks.spectralnorm import SpectralNormConv2d
-from NeuralBlocks.blocks.weightnorm import WeightNormConv2d
 
 class ConvNormRelu(nn.Module):
 
@@ -14,9 +11,9 @@ class ConvNormRelu(nn.Module):
                  groups_size=16, conv_last = False, act = 'relu'):
         super(ConvNormRelu, self).__init__()
 
-        if norm not in [None,'BN', 'IN', 'GN', 'LN','WN', 'SN','MWN','MSN', 'MSNTReLU', 'MWNTReLU']:
+        if norm not in [None,'BN', 'ABN', 'IN', 'GN', 'LN','WN', 'SN','MWN','MSN', 'MSNTReLU', 'MWNTReLU']:
             raise ValueError("Undefined norm value. Must be one of "
-                             "[None,'BN', 'IN', 'GN', 'LN', 'WN', 'SN','MWN','MSN', 'MSNTReLU', 'MWNTReLU']")
+                             "[None,'BN', 'ABN','IN', 'GN', 'LN', 'WN', 'SN','MWN','MSN', 'MSNTReLU', 'MWNTReLU']")
 
         def act_fn(act):
             if act == 'relu':
@@ -27,75 +24,26 @@ class ConvNormRelu(nn.Module):
                 raise ValueError('Undefined activation function.')
 
         layers = []
-        if norm == 'MSN':
-            conv2d = MeanSpectralNormConv2d(in_channels, out_channels, kernel_size,
-                 stride, padding, dilation, groups,
-                 bias, padding_mode)
-            layers += [conv2d, act_fn(act)]
-        elif norm == 'MSNTReLU':
+        if norm == 'MSNTReLU':
             conv2d = MeanSpectralNormConvReLU(in_channels, out_channels, kernel_size,
                  stride, padding, dilation, groups,
                  bias, padding_mode)
             layers += [conv2d]
-        elif norm == 'SN':
-            conv2d = SpectralNormConv2d(in_channels, out_channels, kernel_size,
-                 stride, padding, dilation, groups,
-                 bias, padding_mode)
-            layers += [conv2d, act_fn(act)]
-        elif norm == 'WN':
-            conv2d = WeightNormConv2d(in_channels, out_channels, kernel_size,
-                                          stride, padding, dilation, groups,
-                                          bias, padding_mode)
-            layers += [conv2d, act_fn(act)]
-        elif norm == 'MWN':
-            conv2d = MeanWeightNormConv2d(in_channels, out_channels, kernel_size,
-                 stride, padding, dilation, groups,
-                 bias, padding_mode)
-            layers += [conv2d, act_fn(act)]
         elif norm == 'MWNTReLU':
             conv2d = MeanWeightNormConvReLU(in_channels, out_channels, kernel_size,
                  stride, padding, dilation, groups,
                  bias, padding_mode)
             layers += [conv2d]
-        elif norm == 'IN':
-            conv2d = nn.Conv2d(in_channels, out_channels, kernel_size,
+        elif norm == 'ABN':
+            conv2d = ConvNorm(in_channels, out_channels, kernel_size,
                  stride, padding, dilation, groups,
-                 bias, padding_mode)
-            layers += [conv2d, nn.InstanceNorm2d(out_channels), act_fn(act)]
-        elif norm == 'GN':
-            conv2d = nn.Conv2d(in_channels, out_channels, kernel_size,
-                 stride, padding, dilation, groups,
-                 bias, padding_mode)
-            layers += [conv2d, nn.GroupNorm(groups_size, out_channels), act_fn(act)]
-        elif norm == 'LN':
-            conv2d = nn.Conv2d(in_channels, out_channels, kernel_size,
-                 stride, padding, dilation, groups,
-                 bias, padding_mode)
-            layers += [conv2d, nn.LayerNorm(groups_size, out_channels), act_fn(act)]
-        elif norm == 'BN':
-            conv2d = nn.Conv2d(in_channels, out_channels, kernel_size,
-                 stride, padding, dilation, groups,
-                 bias, padding_mode)
-            layers += [conv2d, nn.BatchNorm2d(out_channels), act_fn(act)]
+                 bias, padding_mode, norm = norm, groups_size=groups_size, conv_last = conv_last)
+            layers += [conv2d]
         else:
-            conv2d = nn.Conv2d(in_channels, out_channels, kernel_size,
-                               stride, padding, dilation, groups,
-                               bias, padding_mode)
+            conv2d = ConvNorm(in_channels, out_channels, kernel_size,
+                 stride, padding, dilation, groups,
+                 bias, padding_mode, norm = norm, groups_size=groups_size, conv_last = conv_last)
             layers += [conv2d, act_fn(act)]
-
-        """
-        conv_last is a flag to change the order of operations from
-            Conv2D+ BN+ReLU to BN+ReLU+Con2D
-        This is frequently used in DenseNet & ResNet architectures.
-        So to change the order, we simply rotate the array by 1 to the 
-        left and change the num_features to the in_channels size
-        """
-        if conv_last:
-            layers = layers[1:] + layers[:1]
-            # Reinitialize the batchnorm layer or its variants
-            if norm in ['BN', 'LN', 'IN', 'GN']:
-                layers[0].__init__(in_channels)
-
         self.layers= nn.Sequential(*layers)
 
     def forward(self, input):
